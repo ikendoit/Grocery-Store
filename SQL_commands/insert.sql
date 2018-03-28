d8a95dfe0b284811fb793ce2bc21ebe8365ce2d3
pboparai01 <parm_b13@hotmail.com>
2018-03-27 17:37:36 -0700

    Info Changed V2
    Changed info in:
    Producers
    Vendors
    Products
    Dependants

--- SQL_commands/insert.sql
+++ SQL_commands/insert.sql
@@ -1,32 +1,32 @@
 INSERT INTO Staff VALUES(
-1, 'Ken N', '1020302827181', '3913 Cates Landing Way', '1998/3/1', 6043669692, 'virginman101@gmail.com', '111222131112', '2017/9/6'
+1, 'Ken N', '143430346', '3913 Cates Landing Way', '1998/3/1', 6047191600, 'virginman101@gmail.com', '5520033435035168', '2017/9/6'
 );
 INSERT INTO Staff VALUES(
-2, 'Gary Y', '1123211127181', '323 something Vancouver', '1912/3/1', '6043569692', 'thisemail2@gmail.com', '155222131112', '2017/9/1'
+2, 'Gary Y', '138347950', '3988 Alness Street', '1912/3/1', 6047206029, 'mthurn@live.com', '5137403916368732', '2017/9/1'
 );
 INSERT INTO Staff VALUES(
-3, 'Parm B', '1220302827181', '313 VC', '1913/3/1', 6043269692, 'thisemail3@gmail.com', '144222131112', '2017/9/2'
+3, 'Parm B', '164791279', '700 184th Street', '1913/3/1', 6043666528, 'fangorn@hotmail.com', '5127175163174482', '2017/9/2'
 );
 INSERT INTO Staff VALUES(
-4, 'Ken F', '1320302827181', '391 Cates Landing Way', '1992/3/1', 6043619692, 'thisemail4@gmail.com', '133222131555', '2017/9/5'
+4, 'Bradley P', '082191289', '1738 St. John Street', '1992/3/1', 6044425489, 'euice@outlook.com', '5221226689506908', '2017/9/5'
 );
 INSERT INTO Staff VALUES(
-5, 'Ken W', '1420302827281', 'add 52', '1991/3/1', 6003629692, 'thisemail5@gmail.com', '122222333112', '2016/9/6'
+5, 'Diane J', '482518123', '1190 Fourth Avenue', '1991/3/1', 7789898303, 'rgarcia@optonline.net', '5404374921536637', '2016/9/6'
 );
 INSERT INTO Staff VALUES(
-6, 'Ken Q', '20302827181', 'add 50', '1991/3/1', 6033629692, 'thisemail@gmail.com', '12333112', '2016/1/6'
+6, 'Albert M', '093507382', '2472 St. John Street', '1991/3/1', 6042801947, 'webdragon@comcast.net', '5324206172373343', '2016/1/6'
 );
 INSERT INTO Staff VALUES(
-7, 'Ken W', '1420302827181', 'add 39', '1989/3/1', 6043629692, 'thisemail10@gmail.com', '129999933112', '2016/2/6'
+7, 'Ken W', '452091192', '2318 Tycos Dr', '1989/3/1', 6049234929, 'mxiao@yahoo.com', '5177433405186197', '2016/2/6'
 );
 INSERT INTO Staff VALUES(
-8, 'Ken WC', '7720302827181', 'add 12', '1991/1/1', 6013629692, 'thisemail11@gmail.com', '9999933112', '2016/12/6'
+8, 'Ruby M', '255165425', '127 Sheppard Ave', '1991/1/1', 6047343929, 'drezet@me.com', '5289002117773645', '2016/12/6'
 );
 INSERT INTO Staff VALUES(
-9, 'Ken WB', '6520302827181', 'add 13', '1991/3/1', 6023629322, 'thisemail12@gmail.com', '219933112', '2016/5/6'
+9, 'David R', '507959773', '1756 Wellington Ave', '1991/3/1', 6047310740, 'miyop@icloud.com', '5324205940851432', '2016/5/6'
 );
 INSERT INTO Staff VALUES(
-10, 'Logger', '5820302827181', 'add 14', '1991/12/1', 603362962, 'thisemail13@gmail.com', '12933112', '2016/7/6'
+10, 'Francis L', '350231072', '1381 MacLaren Street', '1991/12/1', 6046481365, 'sakusha@yahoo.ca', '5177430140201414', '2016/7/6'
 ); 
 
 
@@ -65,70 +65,70 @@ INSERT INTO Manager VALUES(
 
 
 INSERT INTO Producers VALUES(
-1, 'prod1@gmail.com', 11111112222, 'prod1name', 'Prod1_owner', '111 vancouver'
+1, 'tenoid@gmail.com', 11111112222, 'tenoid', 'Leonora C Golden', '4033 Robson St'
 );
 INSERT INTO Producers VALUES(
-2, 'prod2@gmail.com', 12211112222, 'prod2name', 'Prod2_owner', '122 vancouver'
+2, 'homonix@gmail.com', 12211112222, 'homonix', 'Jose J Avery', '3367 Hardy Street'
 );
 INSERT INTO Producers VALUES(
-3, 'prod3@gmail.com', 11111113333, 'prod3name', 'Prod3_owner', '133 vancouver'
+3, 'biomm@gmail.com', 11111113333, 'biomm', 'Joseph M Yim', '4240 Nootka Street'
 );
 INSERT INTO Producers VALUES(
-4, 'prod4@gmail.com', 11111114444, 'prod4name', 'Prod4_owner', '144 vancouver'
+4, 'redel@gmail.com', 11111114444, 'redel', 'Georgie M Cornelison', '2912 Hastings Street'
 );
 INSERT INTO Producers VALUES(
-5, 'prod5@gmail.com', 11111115555, 'prod5name', 'Prod5_owner', '155 vancouver'
+5, 'telente@gmail.com', 11111115555, 'telente', 'Keith A Williamson', '1344 Robson St'
 );
 
 
 INSERT INTO Products VALUES(
-1, 10, 3, 110, 'Cake 1', 19
+1, 10, 3, 110, 'MP3', 19
 );
 INSERT INTO Products VALUES(
-2, 21, 4, 112, 'Cake 2', 20
+2, 21, 4, 112, 'Shirts', 20
 );
 INSERT INTO Products VALUES(
-3, 16, 6, 113, 'Cake 3', 23
+3, 16, 6, 113, 'Small Toy Bear', 23
 );
 INSERT INTO Products VALUES(
-4, 44, 14, 14, 'Cake 4', 24
+4, 44, 14, 14, 'Dumbell', 24
 );
 INSERT INTO Products VALUES(
-5, 25, 5, 115, 'Cake 5', 35
+5, 25, 5, 115, 'Package of Paper', 35
 );
 
 
 INSERT INTO Vendors VALUES(
-1, 'Vend1@gmail.com', 1111111111, 'Vendor 1 Name', 'Vendor 1 Owner', 'Ven1 Address'
+1, 'mistita@gmail.com', 1111111111, 'mistita', 'Andrew K Coleman', '1706 Robson St'
 );
 INSERT INTO Vendors VALUES(
-2, 'Vend2@gmail.com', 2211111111, 'Vendor 2 Name', 'Vendor 2 Owner', 'Ven2 Address'
+2, 'dezz@gmail.com', 2211111111, 'dezz', 'Iris A Twilley', '2636 rue Levy'
 );
 INSERT INTO Vendors VALUES(
-3, 'Vend3@gmail.com', 3311111111, 'Vendor 3 Name', 'Vendor 3 Owner', 'Ven3 Address'
+3, 'conore@gmail.com', 3311111111, 'conore', 'David M Hines', '4626 St Marys Rd'
 );
 INSERT INTO Vendors VALUES(
-4, 'Vend4@gmail.com', 4411111111, 'Vendor 4 Name', 'Vendor 4 Owner', 'Ven4 Address'
+4, 'dure@gmail.com', 4411111111, 'dure', 'Nicole W Rodriguez', '2846 Weston Rd'
 );
 INSERT INTO Vendors VALUES(
-5, 'Vend5@gmail.com', 5511111111, 'Vendor 5 Name', 'Vendor 5 Owner', 'Ven5 Address'
+5, 'supent@gmail.com', 5511111111, 'supent', 'Antonio E Wells', '4624 Exmouth Street'
 );
 
 
 INSERT INTO Dependants VALUES(
-1, 110124111, 'Dep 1', '1967/3/1'
+1, 073345928, 'Hilario N', '1967/3/1'
 );
 INSERT INTO Dependants VALUES(
-2, 220112111, 'Dep 2', '1992/3/1'
+2, 667107619, 'Brian Y', '1992/3/1'
 );
 INSERT INTO Dependants VALUES(
-3, 330212111, 'Dep 3', '1963/3/1'
+3, 594750267, 'Tracie B', '1963/3/1'
 );
 INSERT INTO Dependants VALUES(
-4, 443312111, 'Dep 4', '1964/3/1'
+4, 479766180, 'Donna P', '1964/3/1'
 );
 INSERT INTO Dependants VALUES(
-5, 551211221, 'Dep 5', '1965/3/1'
+5, 311901268, 'Joseph J', '1965/3/1'
 );
 
 
